f915386ebc7b134f8d5c2f94a59ac6801777f174
Shahbaz Ali <shahbazaliengineer1@gmail.com>
2020-12-10 20:54:18 +0500

    print song

---
+++ print_song.py
@@ -0,0 +1,36 @@
+import time
+song = """
+Oh, hush, my dear, it's been a difficult year \n
+And terrors don't prey on\n
+Innocent victims\n
+Trust me, darling, trust me darling\n
+It's been a loveless year\n
+I'm a man of three fears\n
+Integrity, faith and\n
+Crocodile tears\n
+Trust me, darling, trust me, darling\n
+So look me in the eyes\n
+Tell me what you see\n
+Perfect paradise\n
+Tearing at the seams\n
+I wish I could escape\n
+I don't wanna fake it\n
+Wish I could erase it\n
+Make your heart believe\n
+But I'm a bad liar, bad liar\n
+Now you know\n
+Now you know\n
+I'm a bad liar, bad liar\n
+Now you know, you're free to go (go)\n
+Did all my dreams never mean one thing?\n
+Does happiness lie in a diamond ring?\n
+Oh, I've been askin'\n
+Oh, I've been askin' for problems, problems, problems\n
+I wage my war, on the world inside\n
+I take my gun to the enemy's side\n
+Oh, I've been\n
+"""
+
+for line in song.splitlines():
+    print(line)
+    time.sleep(0.5)
